c42bc0d15a784ad68617d155ab7bb769d9a69100
Jaka <kraljkavce@gmail.com>
2024-01-11 15:56:45 +0100

    test gh actions

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get('/', async (req, res) => {
     try {
         const response = await fetch('https://www.google.com');
         if (response.ok) {
-            res.send('OK');
+            res.send('OK test');
         } else {
             res.send('Not OK');
         }
